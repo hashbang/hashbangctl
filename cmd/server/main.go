dcf1bdf73ee4de4034eef5ceeca6cdff3ff320cd
Reynir Björnsson <reynir@reynir.dk>
2020-01-08 13:38:50 +0100

    Retab spaces into tabs

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -1,17 +1,17 @@
 package main
 
 import (
-    "io"
-    "fmt"
-    "os"
-    "strings"
-    "os/exec"
-    "syscall"
-    "unsafe"
-    "log"
-    "github.com/gliderlabs/ssh"
-    "github.com/kr/pty"
-    gossh "golang.org/x/crypto/ssh"
+	"io"
+	"fmt"
+	"os"
+	"strings"
+	"os/exec"
+	"syscall"
+	"unsafe"
+	"log"
+	"github.com/gliderlabs/ssh"
+	"github.com/kr/pty"
+	gossh "golang.org/x/crypto/ssh"
 )
 
 func setWinsize(f *os.File, w, h int) {
@@ -20,44 +20,44 @@ func setWinsize(f *os.File, w, h int) {
 }
 
 func main() {
-    ssh.Handle(func(s ssh.Session) {
-	    ptyReq, winCh, isPty := s.Pty()
-	    if !isPty {
-	        io.WriteString(s, "only interactive terminals are supported")
-	        s.Exit(1)
-	        return
-	    }
-        cmd := exec.Command("/client")
-        cmd.Env = append(cmd.Env, fmt.Sprintf("TERM=%s", ptyReq.Term))
+	ssh.Handle(func(s ssh.Session) {
+		ptyReq, winCh, isPty := s.Pty()
+		if !isPty {
+			io.WriteString(s, "only interactive terminals are supported")
+			s.Exit(1)
+			return
+		}
+		cmd := exec.Command("/client")
+		cmd.Env = append(cmd.Env, fmt.Sprintf("TERM=%s", ptyReq.Term))
 
-        user := s.User()
-        cmd.Env = append(cmd.Env, fmt.Sprintf("USERNAME=%s", ptyReq.Term))
+		user := s.User()
+		cmd.Env = append(cmd.Env, fmt.Sprintf("USERNAME=%s", ptyReq.Term))
 
-        publicKey := strings.TrimSpace(string(gossh.MarshalAuthorizedKey(s.PublicKey())))
-        cmd.Env = append(cmd.Env, fmt.Sprintf("PUBLIC_KEY=%s", publicKey))
+		publicKey := strings.TrimSpace(string(gossh.MarshalAuthorizedKey(s.PublicKey())))
+		cmd.Env = append(cmd.Env, fmt.Sprintf("PUBLIC_KEY=%s", publicKey))
 
-        log.Println(fmt.Sprintf("Connection: %s %s", publicKey, user))
+		log.Println(fmt.Sprintf("Connection: %s %s", publicKey, user))
 
-	    f, err := pty.Start(cmd)
-	    if err != nil {
-	    	panic(err)
-	    }
-	    go func() {
-	    	for win := range winCh {
-	    		setWinsize(f, win.Width, win.Height)
-	    	}
-	    }()
-	    go func() {
-	    	io.Copy(f, s) // stdin
-	    }()
-	    io.Copy(s, f) // stdout
-	    cmd.Wait()
+		f, err := pty.Start(cmd)
+		if err != nil {
+			panic(err)
+		}
+		go func() {
+			for win := range winCh {
+				setWinsize(f, win.Width, win.Height)
+			}
+		}()
+		go func() {
+			io.Copy(f, s) // stdin
+		}()
+		io.Copy(s, f) // stdout
+		cmd.Wait()
 
-    })
-    publicKeyOption := ssh.PublicKeyAuth(func(ctx ssh.Context, key ssh.PublicKey) bool {
+	})
+	publicKeyOption := ssh.PublicKeyAuth(func(ctx ssh.Context, key ssh.PublicKey) bool {
 		return true
 	})
 
-    log.Println("starting ssh server on port 2222...")
-    log.Fatal(ssh.ListenAndServe(":2222", nil, publicKeyOption))
+	log.Println("starting ssh server on port 2222...")
+	log.Fatal(ssh.ListenAndServe(":2222", nil, publicKeyOption))
 }
