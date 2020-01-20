f0297953d3ee0ff7e4ebb5bd20afae79719c88a0
Lance R. Vick <lance@lrvick.net>
2020-01-20 00:51:31 -0800

    make my editor sane and autoreformat go code

--- cmd/client/main.go
+++ cmd/client/main.go
@@ -1,41 +1,41 @@
 package main
 
 import (
-    "os"
-    "fmt"
-    "github.com/gdamore/tcell"
-    "github.com/rivo/tview"
+	"fmt"
+	"github.com/gdamore/tcell"
+	"github.com/rivo/tview"
+	"os"
 )
 
-func getUsername(){
-    // Modify input username to be unix compatible
-    // if result is available, return
-    // If not, append random 4 digit number then return
-    //return true
+func getUsername() {
+	// Modify input username to be unix compatible
+	// if result is available, return
+	// If not, append random 4 digit number then return
+	//return true
 }
 
-func createAccount(server string, user string, key string){
-    // Create account
-    // trigger rendering of results and exit button
-    //log.Println(server, user, key)
+func createAccount(server string, user string, key string) {
+	// Create account
+	// trigger rendering of results and exit button
+	//log.Println(server, user, key)
 }
 
 func main() {
-    if os.Getenv("KEY") == "none" {
-        fmt.Fprintln(
-            os.Stderr,
-            "\nError: Public key authentication required\n",
-            "\nFor help generating a key try:\n",
-            "\n$ ssh-keygen -t ed25519 -f \"$HOME/.ssh/id_ed25519\"\n",
-        )
-        os.Exit(1)
-    }
+	if os.Getenv("KEY") == "none" {
+		fmt.Fprintln(
+			os.Stderr,
+			"\nError: Public key authentication required\n",
+			"\nFor help generating a key try:\n",
+			"\n$ ssh-keygen -t ed25519 -f \"$HOME/.ssh/id_ed25519\"\n",
+		)
+		os.Exit(1)
+	}
 
-    app := tview.NewApplication()
+	app := tview.NewApplication()
 
-    logo := tview.NewTextView()
-    logo.SetTextAlign(1)
-    logo.SetText(`
+	logo := tview.NewTextView()
+	logo.SetTextAlign(1)
+	logo.SetText(`
      █████   █████       █████
      █████   █████       █████
      █████   █████       █████
@@ -51,49 +51,51 @@ func main() {
 
 `)
 
-    frame := tview.NewFrame(func() tview.Primitive {
-        form := tview.NewForm()
-        form.SetLabelColor(tcell.ColorWhite)
-        form.SetItemPadding(2)
-        form.SetFieldTextColor(tcell.ColorGray)
-        form.SetButtonTextColor(tcell.ColorGray)
-        form.SetFieldBackgroundColor(tcell.ColorWhite)
-        form.SetButtonBackgroundColor(tcell.ColorWhite)
-        form.SetBorder(false)
-        form.SetButtonsAlign(1)
-        form.AddDropDown("Server",
-            []string{"de1.hashbang.sh","la1.hashbang.sh"}, 0, nil,
-        )
-        form.AddInputField("User Name",
-            os.Getenv("USER"), 33, tview.InputFieldMaxLength(30), nil,
-        )
-        form.AddInputField("Public Key",
-            os.Getenv("KEY"), 33, tview.InputFieldMaxLength(800), nil,
-        )
-        form.AddButton("Create", func(){
-            server_dropdown := form.GetFormItem(0).(*tview.DropDown)
-            _, server := server_dropdown.GetCurrentOption()
-            createAccount(
-                server,
-                form.GetFormItem(1).(*tview.InputField).GetText(),
-                form.GetFormItem(2).(*tview.InputField).GetText(),
-            )
-        })
-        form.AddButton("Exit", app.Stop )
-        return form
-    }())
-    frame.SetBorder(false)
+	frame := tview.NewFrame(func() tview.Primitive {
+		form := tview.NewForm()
+		form.SetLabelColor(tcell.ColorWhite)
+		form.SetItemPadding(2)
+		form.SetFieldTextColor(tcell.ColorGray)
+		form.SetButtonTextColor(tcell.ColorGray)
+		form.SetFieldBackgroundColor(tcell.ColorWhite)
+		form.SetButtonBackgroundColor(tcell.ColorWhite)
+		form.SetBorder(false)
+		form.SetButtonsAlign(1)
+		form.AddDropDown("Server",
+			[]string{"de1.hashbang.sh", "la1.hashbang.sh"}, 0, nil,
+		)
+		form.AddInputField("User Name",
+			os.Getenv("USER"), 33, tview.InputFieldMaxLength(30), nil,
+		)
+		form.AddInputField("Public Key",
+			os.Getenv("KEY"), 33, tview.InputFieldMaxLength(800), nil,
+		)
+		form.AddButton("Create", func() {
+			server_dropdown := form.GetFormItem(0).(*tview.DropDown)
+			_, server := server_dropdown.GetCurrentOption()
+			createAccount(
+				server,
+				form.GetFormItem(1).(*tview.InputField).GetText(),
+				form.GetFormItem(2).(*tview.InputField).GetText(),
+			)
+		})
+		form.AddButton("Exit", app.Stop)
+		return form
+	}())
+	frame.SetBorder(false)
 
-    flex := tview.NewFlex()
-    flex.AddItem(tview.NewBox(), 0, 1, false)
-    flex.AddItem(
-        tview.NewFlex().SetDirection(tview.FlexRow).
-            AddItem(tview.NewBox(), 0, 1, false).
-            AddItem(logo, 14, 1, false).
-            AddItem(frame, 14, 1, true).
-            AddItem(tview.NewBox(), 0, 1, false),
-    50, 2, true)
-    flex.AddItem(tview.NewBox(), 0, 1, false)
+	flex := tview.NewFlex()
+	flex.AddItem(tview.NewBox(), 0, 1, false)
+	flex.AddItem(
+		tview.NewFlex().SetDirection(tview.FlexRow).
+			AddItem(tview.NewBox(), 0, 1, false).
+			AddItem(logo, 14, 1, false).
+			AddItem(frame, 14, 1, true).
+			AddItem(tview.NewBox(), 0, 1, false),
+		50, 2, true)
+	flex.AddItem(tview.NewBox(), 0, 1, false)
 
-    if err := app.SetRoot(flex, true).Run(); err != nil { panic(err) }
+	if err := app.SetRoot(flex, true).Run(); err != nil {
+		panic(err)
+	}
 }
--- cmd/server/main.go
+++ cmd/server/main.go
@@ -1,143 +1,143 @@
 package main
 
 import (
-    "log"
-    "net"
-    "crypto/rsa"
-    "crypto/rand"
-    "io"
-    "sync"
-    "fmt"
-    "strings"
-    "syscall"
-    "unsafe"
-    "os"
-    "os/exec"
-    "path/filepath"
-    "encoding/binary"
-    "golang.org/x/crypto/ssh"
-    "github.com/kr/pty"
+	"crypto/rand"
+	"crypto/rsa"
+	"encoding/binary"
+	"fmt"
+	"github.com/kr/pty"
+	"golang.org/x/crypto/ssh"
+	"io"
+	"log"
+	"net"
+	"os"
+	"os/exec"
+	"path/filepath"
+	"strings"
+	"sync"
+	"syscall"
+	"unsafe"
 )
 
 func setWinsize(fd uintptr, w, h uint32) {
-    syscall.Syscall(
-        syscall.SYS_IOCTL,
-        fd,
-        uintptr(syscall.TIOCSWINSZ),
-        uintptr(unsafe.Pointer(
-            &struct{ h, w, x, y uint16 }{uint16(h), uint16(w), 0, 0},
-        )),
-    )
+	syscall.Syscall(
+		syscall.SYS_IOCTL,
+		fd,
+		uintptr(syscall.TIOCSWINSZ),
+		uintptr(unsafe.Pointer(
+			&struct{ h, w, x, y uint16 }{uint16(h), uint16(w), 0, 0},
+		)),
+	)
 }
 
 func parseDims(b []byte) (uint32, uint32) {
-    w := binary.BigEndian.Uint32(b)
-    h := binary.BigEndian.Uint32(b[4:])
-    return w, h
+	w := binary.BigEndian.Uint32(b)
+	h := binary.BigEndian.Uint32(b[4:])
+	return w, h
 }
 
 func ptyRun(c *exec.Cmd, tty *os.File) (err error) {
-    defer tty.Close()
-    c.Stdout = tty
-    c.Stdin = tty
-    c.Stderr = tty
-    c.SysProcAttr = &syscall.SysProcAttr{
-        Setctty: true,
-        Setsid:  true,
-    }
-    return c.Start()
+	defer tty.Close()
+	c.Stdout = tty
+	c.Stdin = tty
+	c.Stderr = tty
+	c.SysProcAttr = &syscall.SysProcAttr{
+		Setctty: true,
+		Setsid:  true,
+	}
+	return c.Start()
 }
 
 func publicKeyCallback(
-    conn ssh.ConnMetadata,
-    key ssh.PublicKey,
+	conn ssh.ConnMetadata,
+	key ssh.PublicKey,
 ) (*ssh.Permissions, error) {
-    return &ssh.Permissions{
-        Extensions: map[string]string{
-            "pubkey": strings.TrimSpace(string(ssh.MarshalAuthorizedKey(key))),
-        },
-    }, nil
+	return &ssh.Permissions{
+		Extensions: map[string]string{
+			"pubkey": strings.TrimSpace(string(ssh.MarshalAuthorizedKey(key))),
+		},
+	}, nil
 }
 
 func keyboardInteractiveCallback(
-    ssh.ConnMetadata,
-    ssh.KeyboardInteractiveChallenge,
+	ssh.ConnMetadata,
+	ssh.KeyboardInteractiveChallenge,
 ) (*ssh.Permissions, error) {
-    return nil, nil
+	return nil, nil
 }
 
 func handleConnection(nConn net.Conn, sshConfig *ssh.ServerConfig) {
-    conn, chans, _, _ := ssh.NewServerConn(nConn, sshConfig)
-    go func(chans <-chan ssh.NewChannel){
-        for newChannel := range chans {
-            if t := newChannel.ChannelType(); t != "session" {
-                newChannel.Reject(
-                    ssh.UnknownChannelType,
-                    fmt.Sprintf("unknown channel type: %s", t),
-                );
-                continue;
-            }
-            channel, requests, _ := newChannel.Accept()
-            f, tty, _ := pty.Open()
-            go func(in <-chan *ssh.Request) {
-                user := conn.Conn.User()
-                ip := fmt.Sprintf("%s", conn.Conn.RemoteAddr())
-                version := fmt.Sprintf("%s", conn.Conn.ClientVersion())
-                key := "none"
-                if conn.Permissions != nil {
-                    if conn.Permissions.Extensions != nil {
-                        if k, ok := conn.Permissions.Extensions["pubkey"]; ok {
-                            key = k
-                        }
-                    }
-                }
-                log.Println("->",ip,version,user,key)
-                for req := range in {
-                    switch req.Type {
-                        case "shell":
-                            runDir, _ := filepath.Abs(filepath.Dir(os.Args[0]))
-                            cmd := exec.Command(
-                                fmt.Sprintf("%s/client", runDir),
-                            )
-                            cmd.Env = append(
-                                os.Environ(),
-                                "TERM=xterm-256color",
-                                fmt.Sprintf("IP=%s", ip),
-                                fmt.Sprintf("VERSION=%s", version),
-                                fmt.Sprintf("USER=%s", user),
-                                fmt.Sprintf("KEY=%s", key),
-                            )
-                            err := ptyRun(cmd, tty)
-                            if err != nil {
-                                log.Printf("%s", err)
-                            }
-                            var once sync.Once
-                            close := func() {
-                                channel.Close()
-                                log.Println("<-",ip,version,user,key)
-                            }
-                            go func() {
-                                io.Copy(channel, f)
-                                once.Do(close)
-                            }()
-                            go func() {
-                                io.Copy(f, channel)
-                                once.Do(close)
-                            }()
-                        case "pty-req":
-                            termLen := req.Payload[3]
-                            w, h := parseDims(req.Payload[termLen+4:])
-                            setWinsize(f.Fd(), w, h)
-                        case "window-change":
-                            w, h := parseDims(req.Payload)
-                            setWinsize(f.Fd(), w, h)
-                            continue
-                    }
-                    req.Reply(true, nil)
-                }
-            }(requests)
-        }
-    }(chans)
+	conn, chans, _, _ := ssh.NewServerConn(nConn, sshConfig)
+	go func(chans <-chan ssh.NewChannel) {
+		for newChannel := range chans {
+			if t := newChannel.ChannelType(); t != "session" {
+				newChannel.Reject(
+					ssh.UnknownChannelType,
+					fmt.Sprintf("unknown channel type: %s", t),
+				)
+				continue
+			}
+			channel, requests, _ := newChannel.Accept()
+			f, tty, _ := pty.Open()
+			go func(in <-chan *ssh.Request) {
+				user := conn.Conn.User()
+				ip := fmt.Sprintf("%s", conn.Conn.RemoteAddr())
+				version := fmt.Sprintf("%s", conn.Conn.ClientVersion())
+				key := "none"
+				if conn.Permissions != nil {
+					if conn.Permissions.Extensions != nil {
+						if k, ok := conn.Permissions.Extensions["pubkey"]; ok {
+							key = k
+						}
+					}
+				}
+				log.Println("->", ip, version, user, key)
+				for req := range in {
+					switch req.Type {
+					case "shell":
+						runDir, _ := filepath.Abs(filepath.Dir(os.Args[0]))
+						cmd := exec.Command(
+							fmt.Sprintf("%s/client", runDir),
+						)
+						cmd.Env = append(
+							os.Environ(),
+							"TERM=xterm-256color",
+							fmt.Sprintf("IP=%s", ip),
+							fmt.Sprintf("VERSION=%s", version),
+							fmt.Sprintf("USER=%s", user),
+							fmt.Sprintf("KEY=%s", key),
+						)
+						err := ptyRun(cmd, tty)
+						if err != nil {
+							log.Printf("%s", err)
+						}
+						var once sync.Once
+						close := func() {
+							channel.Close()
+							log.Println("<-", ip, version, user, key)
+						}
+						go func() {
+							io.Copy(channel, f)
+							once.Do(close)
+						}()
+						go func() {
+							io.Copy(f, channel)
+							once.Do(close)
+						}()
+					case "pty-req":
+						termLen := req.Payload[3]
+						w, h := parseDims(req.Payload[termLen+4:])
+						setWinsize(f.Fd(), w, h)
+					case "window-change":
+						w, h := parseDims(req.Payload)
+						setWinsize(f.Fd(), w, h)
+						continue
+					}
+					req.Reply(true, nil)
+				}
+			}(requests)
+		}
+	}(chans)
 }
 
 var (
@@ -145,24 +145,29 @@ var (
 )
 
 func init() {
-    key, _ := rsa.GenerateKey(rand.Reader, 2048)
-    hostPrivateKeySigner, _ = ssh.NewSignerFromKey(key)
+	key, _ := rsa.GenerateKey(rand.Reader, 2048)
+	hostPrivateKeySigner, _ = ssh.NewSignerFromKey(key)
 }
 
 func main() {
 
-    sshConfig := &ssh.ServerConfig{
+	sshConfig := &ssh.ServerConfig{
 		KeyboardInteractiveCallback: keyboardInteractiveCallback,
-		PublicKeyCallback: publicKeyCallback,
+		PublicKeyCallback:           publicKeyCallback,
 	}
-    sshConfig.AddHostKey(hostPrivateKeySigner)
+	sshConfig.AddHostKey(hostPrivateKeySigner)
 
-    listener, err := net.Listen("tcp4", ":2222")
-    if err != nil { panic(err) }
-    for {
-        conn, err := listener.Accept()
-        if err != nil { log.Println("!! %s", err); continue; }
-        go handleConnection(conn, sshConfig)
-    }
+	listener, err := net.Listen("tcp4", ":2222")
+	if err != nil {
+		panic(err)
+	}
+	for {
+		conn, err := listener.Accept()
+		if err != nil {
+			log.Println("!! %s", err)
+			continue
+		}
+		go handleConnection(conn, sshConfig)
+	}
 
 }
